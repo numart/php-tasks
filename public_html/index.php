c3f237768388442a477dd438728d2da84f0d35d2
Nuria Martinez <numartinez@hiberus.com>
2025-04-13 19:26:25 +0200

    feat: add index html

---
+++ public_html/index.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Tasks</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+
+<header class=" py-3 mb-4 border-bottom">
+  <div class="container">
+    <div class="d-flex flex-wrap justify-content-center">
+
+      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+        <svg class="bi me-2" width="40" height="32">
+          <use xlink:href="#bootstrap"></use>
+        </svg>
+        <span class="fs-4">Simple header</span>
+      </a>
+
+      <ul class="nav nav-pills">
+        <li class="nav-item">
+          <a href="#" class="nav-link active" aria-current="page">Home</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+      </ul>
+    </div>
+  </div>
+</header>
+
+<section class="container">
+  <div class="row">
+    <div class="col">
+      <h1>Tareas</h1>
+
+      <table>
+        <thead>
+        <td>Nombre</td>
+        <td>Estado</td>
+        <td>Fecha</td>
+        <td>Acciones</td>
+        </thead>
+      </table>
+
+    </div>
+  </div>
+</section>
+
+</body>
+</html>
+
