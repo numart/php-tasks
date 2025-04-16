b7b4dd077d446f5fa65aa981f0466f611966d553
numart <numartinez@hiberus.com>
2025-04-16 20:00:54 +0200

    feat: add layout template

--- public/templates/tasks.php
+++ public/templates/tasks.php
@@ -1,55 +1,22 @@
-<!doctype html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Tasks</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-</head>
-<body>
+<?php
+ob_start(); ?>
 
-<header class=" py-3 mb-4 border-bottom">
-  <div class="container">
-    <div class="d-flex flex-wrap justify-content-center">
+  <h1>Tareas</h1>
+  <a href="/new-task" type="button" class="btn btn-primary">Crear tarea</a>
 
-      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-        <svg class="bi me-2" width="40" height="32">
-          <use xlink:href="#bootstrap"></use>
-        </svg>
-        <span class="fs-4">Simple header</span>
-      </a>
+<?php
+if (!empty($tasks)): ?>
+  <ul>
+      <?php
+      foreach ($tasks as $task): ?>
+        <li><?= $task->getTitle().' - '.$task->getDate(); ?></li>
 
-      <ul class="nav nav-pills">
-        <li class="nav-item">
-          <a href="#" class="nav-link active" aria-current="page">Home</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">About</a></li>
-      </ul>
-    </div>
-  </div>
-</header>
+      <?php
+      endforeach; ?>
+  </ul>
+<?php
+endif; ?>
 
-<section class="container">
-  <div class="row">
-    <div class="col">
-      <a href="/new-task" type="button" class="btn btn-primary">Crear tarea</a>
-      <h1>Tareas</h1>
-
-        <?php if (!empty($tasks)): ?>
-        <ul>
-            <?php foreach ($tasks as $task): ?>
-              <li><?= $task->getTitle() . ' - ' . $task->getDate(); ?></li>
-
-            <?php endforeach; ?>
-        </ul>
-        <?php endif; ?>
-
-    </div>
-  </div>
-</section>
-
-</body>
-</html>
+<?php
+$content = ob_get_clean();
+require 'layout.php';
